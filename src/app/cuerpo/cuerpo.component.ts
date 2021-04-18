8f1a99dfa35ee0d66219d8eb73b04ceb9f461315
Saquipay <cristian.saquipay.est@tecazuay.edu.ec>
2021-04-18 13:47:31 -0500

    Instalacion de google maps

--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -7,7 +7,8 @@ import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
   styleUrls: ['./cuerpo.component.css']
 })
 export class CuerpoComponent implements OnInit {
- 
+ /*Instalar el google maps 
+ ingresar en la terminal y colocar npm install @angular/google-maps*/ 
   center = {lat: -2.955152344389949, lng:  -78.9764076271043};
   zoom = 17;
   mapTypeId = "hibrid";
